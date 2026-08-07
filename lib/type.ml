8792cb09aa6a2e2e6296dc87919f0adff520ffd2
milesng <miles.c.y.ng@gmail.com>
2026-08-07 20:47:05 +1000

    unify scratch

--- lib/type.ml
+++ lib/type.ml
@@ -37,7 +37,7 @@ end
 open StaticEnvironment
 open Ast
 
-let apply_sub ty subs =
+let apply_subs ty subs =
   match ty with
   | TVar var -> (
     match IntMap.find_opt var subs with
@@ -48,7 +48,31 @@ let apply_sub ty subs =
 
 let instantiate (Forall (vars, ty)) =
   let subs = List.fold_left (fun map var -> IntMap.add var (fresh_tvar ()) map) empty_sub vars in
-  apply_sub ty subs
+  apply_subs ty subs
+
+let occurs var ty = true
+
+let bind_tvar subs var ty =
+  let ty = apply_subs ty subs in
+
+  if ty = TVar var then
+    subs
+  else if occurs var ty then
+    failwith "Infinite type."
+  else IntMap.add var ty subs
+
+let rec unify subs t1 t2 =
+  let t1 = apply_subs t1 subs in
+  let t2 = apply_subs t2 subs in
+
+  match t1, t2 with
+  | TInt, TInt | TBool, TBool | TUnit, TUnit -> Ok subs
+  | TVar a, TVar b when a = b -> Ok subs
+  | TVar a, TInt | TInt, TVar a -> Ok (IntMap.add a TInt subs)
+  | TVar a, TBool | TBool, TVar a -> IntMap.add a TBool empty_sub
+  | TVar a, TUnit | TUnit, TVar a -> IntMap.add a TUnit empty_sub
+  | TFun (arg1, body1), TFun (arg2, body2) ->
+    let s1 = unify arg1 arg2 in
 
 let rec infer env = function
   | Int _ -> empty_sub, TInt
@@ -59,27 +83,28 @@ let rec infer env = function
     let ty = instantiate scheme in
     empty_sub, ty
   | Fun (arg, e) ->
-    let arg_ty = fresh_tvar () in
-    let env' = extend env arg arg_ty in
+    let a = fresh_tvar () in
+    let env' = extend env arg (Forall ([], a)) in
     let subs, body_ty = infer env' e in
+    let arg_ty = apply_subs a subs in
     subs, TFun (arg_ty, body_ty)
   | Let (x, e1, e2) | LetRec (x, e1, e2) ->
       let _, t1 = infer env e1 in
-      let env' = extend env x t1 in
+      let env' = extend env x (Forall ([], t1)) in
       infer env' e2
   | Binop (bop, e1, e2) -> (
-      let t1 = infer env e1 in
-      let t2 = infer env e2 in
+      let _, t1 = infer env e1 in
+      let _, t2 = infer env e2 in
       match (bop, t1, t2) with
       | Add, TInt, TInt | Mul, TInt, TInt | Div, TInt, TInt | Sub, TInt, TInt ->
-          TInt
-      | Eq, TInt, TInt | Lt, TInt, TInt | Gt, TInt, TInt -> TBool
+          empty_sub, TInt
+      | Eq, TInt, TInt | Lt, TInt, TInt | Gt, TInt, TInt -> empty_sub, TBool
       | _ -> failwith "Binary operator and operand type mismatch.")
   | Unop (op, e) -> (
-      let ty = infer env e in
+      let subs, ty = infer env e in
       match (op, ty) with
-      | BNeg, TBool -> TBool
-      | Neg, TInt -> TInt
+      | BNeg, TBool -> empty_sub, TBool
+      | Neg, TInt -> empty_sub, TInt
       | _ -> failwith "Unary operator and operand type mismatch.")
   | If (e1, e2, e3) ->
       if infer env e1 = TBool then
@@ -87,13 +112,11 @@ let rec infer env = function
         if t2 = infer env e3 then t2
         else failwith "Branches of if must have same type."
       else failwith "Guard of if must have type bool."
-  | Apply (e1, e2) -> (
-      let t1 = infer env e1 in
-      let t2 = infer env e2 in
-      match t1 with
-      | TFun (arg, body) when arg = t2 -> body
+  | Apply (f, arg) -> (
+      let s1, f_ty = infer env f in
+      let s2, arg_ty = infer env arg in
+      let arg_ty' = apply_subs arg_ty s1 in
+      match f_ty with
+      | TFun (arg, body) when arg = arg_ty' -> s2, apply_subs body s2
+      | TFun _ -> failwith "Mismatched argument types."
       | _ -> failwith "Applying non function type.")
-
-and unify = function
-  | TUnit, TUnit | TInt, TInt | TBool, TBool -> ()
-  | TVar a, TInt
